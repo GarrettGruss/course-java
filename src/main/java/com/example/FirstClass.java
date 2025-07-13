82eb51e2aeec5303fbab99d291856169f507a16b
Garrett Gruss <garrett.gruss@gmail.com>
2025-07-12 20:37:06 -0500

    section 4

--- src/main/java/com/example/FirstClass.java
+++ src/main/java/com/example/FirstClass.java
@@ -2,6 +2,27 @@ package com.example;
 
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.print("Hello World");
+        System.out.println("Hello, Tim");
+
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an alien");
+            System.out.println("And I am afraid of aliens");
+        }
+
+        int topScore = 80;
+        if (topScore < 100) {
+            System.out.println("You got the top score!");
+        }
+
+        int secondTopScore = 81;
+        if ((topScore > secondTopScore) && (topScore < 100)){
+            System.out.println("Greater than second top score and less than 100");
+        }
+
+        if ((topScore > 90) || (secondTopScore <= 90)) {
+            System.out.println("Either or both of the conditions are true");
+        }
+
     }
 }
